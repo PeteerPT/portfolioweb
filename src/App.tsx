57ca5807037a86c3432eb17e309513b6188bed20
Pedro Oliveira <pedro_10pedro@hotmail.com>
2025-07-04 07:30:33 +0100

    Update App.tsx
    update para receber sons de clicks na página

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
 import Desktop from './components/os/Desktop';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const handler = () => {
+            window.parent.postMessage('playClickSound', '*');
+        };
+        document.body.addEventListener('mousedown', handler);
+        return () => document.body.removeEventListener('mousedown', handler);
+    }, []);
+
     return (
         <div className="App">
             <Desktop />
