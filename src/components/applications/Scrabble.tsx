1799152c673e6858cef7547953fda00bfadd4c40
Pedro Oliveira <pedro_10pedro@hotmail.com>
2025-07-04 07:51:00 +0100

    Update Scrabble.tsx
    update scrabble

--- src/components/applications/Scrabble.tsx
+++ src/components/applications/Scrabble.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+// src/apps/ScrabbleApp.tsx
+
+import React, { useState, useEffect } from 'react';
 import DosPlayer from '../dos/DosPlayer';
 import Window from '../os/Window';
 
@@ -7,6 +9,14 @@ export interface ScrabbleAppProps extends WindowAppProps {}
 const ScrabbleApp: React.FC<ScrabbleAppProps> = (props) => {
     const [width, setWidth] = useState(920);
     const [height, setHeight] = useState(750);
+
+    // Toca o som ao abrir a janela
+    useEffect(() => {
+      const audio = new window.Audio("/sound-opengames.mp3");
+      audio.currentTime = 0;
+      audio.volume = 0.93;
+      audio.play().catch(() => {});
+    }, []);
 
     return (
         <Window
